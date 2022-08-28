d91c9023c1d782b03323a51b1769cc5985500b59
Rupali Shah <rupalishah040@gmail.com>
2022-08-28 10:32:45 +0530

    data access

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
   const[file,setF] = useState("");
   const[pName,setName] = useState("");
   const[pId,setId] = useState("");
+  const[name,setPName] = useState("");
+  const[a,setAccess] = useState("");
   
   const[cName,setCName] = useState("");
   const[cId,setCId] = useState("");
@@ -23,9 +25,17 @@ function App() {
   async function addData(did){
     console.log(window.ethereum.selectedAddress);
     const Ref = collection(db, "users");
-    await setDoc(doc(Ref, "0x74073f9D9e4037fA31f8088e370863623c3Eb501"), {
-      did: did });
+    await setDoc(doc(Ref, window.ethereum.selectedAddress), {
+      dids: did });
     
+  }
+  async function getData(name){
+    const docRef = doc(db, "Research", name);
+    console.log(docRef)
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data() 
+    console.log(data);
+    setAccess(data.wallet_Add);
   }
   return (
     <div className="App">
@@ -35,17 +45,20 @@ function App() {
       {
         pro === "register" ? <div>
         <h1>Register Yourself !!! Who are you ?</h1>
+        
         <button onClick={()=>{setChoice("patient")}}>Patient</button>
         <button onClick={()=>{setChoice("res")}}>Research Company</button>
         {
           choice === "patient" ? <div><h2>Welcome New Patient</h2><form><input type="text" name="pId" onChange={(e)=>{setId(e.target.value)}} placeholder="Registration Id"></input><input type="text" onChange={(e)=>{console.log(e.target.value);setName(e.target.value)}} placeholder="Patient Name"></input> <button onClick={(e)=>{e.preventDefault(); console.log(pName,pId)}}>Add Patient</button></form></div> : choice === "res" ? <div><h2>Welcome New Company</h2><form><input type="text" name="cId" onChange={(e)=>{setCId(e.target.value)}} placeholder="Comapany Id"></input><input type="text" onChange={(e)=>{console.log(e.target.value);setCName(e.target.value)}} placeholder="Company Name"></input> <button onClick={(e)=>{e.preventDefault(); console.log(cName,cId)}}>Add Comapany</button></form></div>  : ""
         } </div>
        : pro === "login" ? <div><h1>Welcome Back !!!</h1>
-        <button onClick={()=>{setChoice("pat"); console.log("Data")}}>Patient</button>
+      
+        <button onClick={()=>{setChoice("pat")}}>Patient</button>
+       
         <button onClick={()=>{setChoice("r")}}>Research Company</button>
         {
           choice === "pat" ? <div><button onClick={async ()=>{
-            
+           // console.log(window.ethereum.getAddress());
 
           const dAppStorageProvider = await new StorageProvider({
             appId: 2193,
@@ -53,7 +66,7 @@ function App() {
             email: "rupalishah040@gmail.com",
           });
           const Downloader = await dAppStorageProvider.getDownloader();
-          const tx = await Downloader.download("0x120ebab31b221665f26b3389f66cc54954aca1f15f45e33fe2242a20972f217f");
+          const tx = await Downloader.download("0x1ef8e0e83e864ec4907b348ba44c8186bb0b8eff4a35c76afa144d3ee681f856");
           alert("Downloaded Check download folder!!")
 
           }}>Get Patient Data</button><input type="file" onChange={(e)=>{setF(e.target.files[0])}}></input><button onClick={async()=>{
@@ -71,7 +84,26 @@ function App() {
             const tx = await Uploader.upload(file);
             console.log(tx);
             addData(tx);
-          }}>Upload New Data</button></div> : choice === "r" ? <div>R</div> : ""
+          }}>Upload New Data</button> <input type="text" onChange={(e)=>{
+            setPName(e.target.value);
+         }}></input>
+         <button onClick={()=>{getData(name)}}>Select Comapany</button><button onClick={async()=>{
+            const dAppStorageProvider = await new StorageProvider({
+              appId:  2193,
+              provider: window.ethereum,
+              email: "rupalishah040@gmail.com",
+            });
+            
+            console.log(dAppStorageProvider);
+            const Access = await new dAppStorageProvider.getAccess();
+            const docRef = doc(db, "users", window.ethereum.selectedAddress);
+            console.log(docRef)
+            const docSnap = await getDoc(docRef);
+            const data = docSnap.data() 
+            console.log(data);
+          //  setAccess(data.wallet_Add);
+            Access.share([data.dids], [a]);
+         }}>Give Access</button></div> : choice === "r" ? <div>R</div> : ""
         }
         </div> : ""
       }
